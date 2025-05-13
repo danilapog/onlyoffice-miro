2d4bf08d52ddb0887f48d1e4e6bf02c0e1ea1e61
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2025-05-13 18:34:37 +0500

    fix(file): add pdf icon

--- frontend/src/features/file/utils/icon.ts
+++ frontend/src/features/file/utils/icon.ts
@@ -4,6 +4,7 @@ export const getIcon = (title?: string) => {
   if (title.endsWith('.docx')) return '/word.svg';
   if (title.endsWith('.xlsx')) return '/cell.svg';
   if (title.endsWith('.pptx')) return '/slide.svg';
+  if (title.endsWith('.pdf')) return '/pdf.svg';
 
   return '/other.svg';
 };
