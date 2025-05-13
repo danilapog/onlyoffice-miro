ab676a6d2df5acda0e05c2a1f2f065d34167fe69
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2025-05-13 18:32:37 +0500

    fix(file): add other icon for non ooxml files

--- frontend/src/features/file/utils/icon.ts
+++ frontend/src/features/file/utils/icon.ts
@@ -1,7 +1,9 @@
 export const getIcon = (title?: string) => {
-  if (!title) return '/word.svg';
+  if (!title) return '/other.svg';
+
+  if (title.endsWith('.docx')) return '/word.svg';
   if (title.endsWith('.xlsx')) return '/cell.svg';
   if (title.endsWith('.pptx')) return '/slide.svg';
 
-  return '/word.svg';
+  return '/other.svg';
 };
